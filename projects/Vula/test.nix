3ea854c2316f646a4810245621b05bbb9f7bb9ee
GetPsyched <priyanshu@getpsyched.dev>
2024-06-10 17:47:42 +0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:projects/Vula/test.nix

--- projects/Vula/test.nix
+++ projects/Vula/test.nix
@@ -86,7 +86,7 @@ in {
     services.xserver = {
       enable = true;
       displayManager.gdm.enable = true;
-      displayManager.gdm.wayland = false;
+      # displayManager.gdm.wayland = false;
       desktopManager.gnome.enable = true;
     };
   };
