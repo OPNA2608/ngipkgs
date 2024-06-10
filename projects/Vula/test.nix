6f3c073db357e83ff12d9097366f39eae3ae9207
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-10 20:55:53 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:projects/Vula/test.nix

--- projects/Vula/test.nix
+++ projects/Vula/test.nix
@@ -86,8 +86,8 @@ in {
     services.xserver = {
       enable = true;
       displayManager.gdm.enable = true;
-      # displayManager.gdm.wayland = false;
-      desktopManager.gnome.enable = true;
+      #desktopManager.gnome.enable = true;
+      desktopManager.xfce.enable = true;
     };
   };
 }
