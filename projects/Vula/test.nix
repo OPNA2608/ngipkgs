745c4578f9b93c827b85ac1f71385582a6d23695
GetPsyched <priyanshu@getpsyched.dev>
2024-06-11 18:01:24 +0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:projects/Vula/test.nix

--- projects/Vula/test.nix
+++ projects/Vula/test.nix
@@ -80,7 +80,7 @@ in {
 
   interactive.nodes.b = {
     virtualisation.memorySize = 4096;
-    virtualisation.cores = 4;
+    virtualisation.cores = 3;
     users.users.joe.isNormalUser = true;
     users.users.joe.password = "";
     users.users.admin.isNormalUser = true;
